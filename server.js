afd87c4552b5338f0454f16342c0d3e0eb06eb73
ezedin ebrahim <ezedinebrahim131@gmail.com>
2024-09-25 21:52:35 +0300

    Update CORS configuration for frontend URL

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://my-graphic-portfolioo.onrender.com", // Allow requests from your frontend URL
+  })
+);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
